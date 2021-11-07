8f3d8ae77b9ba1b47b5e1bd0d32073185923414e
Konstantin Karpov <karp.karpov981@gmail.com>
2021-11-07 11:24:30 +0300

    Add making employee increased and liked

--- src/components/employee-add-form/EmployeeAddForm.tsx
+++ src/components/employee-add-form/EmployeeAddForm.tsx
@@ -1,27 +1,62 @@
+import { ChangeEvent, Component } from 'react';
 import './EmployeeAddForm.css';
 
-function EmployeeAddForm() {
-  return (
-    <div className="app-add form">
-      <h3>Добавьте нового сотрудника</h3>
-      <form className="add-form d-flex">
-        <input
-          type="text"
-          className="form-control new-post-label"
-          placeholder="Как его зовут?"
-        />
-        <input
-          type="number"
-          className="form-control new-post-label"
-          placeholder="З/П в долларах?"
-        />
+interface AddFormProp {}
+interface AddFormState {
+  name: string;
+  salary: number | null;
+}
+class EmployeeAddForm extends Component {
+  constructor(props: AddFormProp) {
+    super(props);
 
-        <button type="submit" className="btn btn-outline-light">
-          Добавить
-        </button>
-      </form>
-    </div>
-  );
+    this.state = {
+      name: '',
+      salary: null,
+    };
+
+    this.setFormData = this.setFormData.bind(this);
+  }
+
+  setFormData(e: ChangeEvent<HTMLInputElement>) {
+    const t = e.target;
+
+    this.setState({
+      [t.name]: t.value,
+    });
+  }
+
+  render() {
+    const { name, salary } = this.state as AddFormState;
+
+    return (
+      <div className="app-add form">
+        <h3>Добавьте нового сотрудника</h3>
+        <form className="add-form d-flex">
+          <input
+            type="text"
+            className="form-control new-post-label"
+            placeholder="Как его зовут?"
+            name="name"
+            value={name}
+            onChange={this.setFormData}
+          />
+          <input
+            type="number"
+            className="form-control new-post-label"
+            placeholder="З/П в долларах?"
+            name="salary"
+            value={salary ? salary : ''}
+            onChange={this.setFormData}
+          />
+
+          <button type="submit" className="btn btn-outline-light">
+            Добавить
+          </button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default EmployeeAddForm;
--- src/components/employee-list-item/EmployeeListItem.tsx
+++ src/components/employee-list-item/EmployeeListItem.tsx
@@ -1,38 +1,81 @@
+import { Component } from 'react';
 import { IData } from '../app/App';
 import './EmployeeListItem.css';
 
 type EmployeeProp = Omit<IData, 'id'>;
 
-function EmployeeListItem({ name, salary = 1000, isIncrease }: EmployeeProp) {
-  const increaseClass = isIncrease ? ' increase' : '';
+interface EmployeeState {
+  isIncrease: boolean;
+  isChoosen: boolean;
+}
 
-  return (
-    <li
-      className={
-        'list-group-item d-flex justify-content-between' + increaseClass
-      }
-    >
-      <span className="list-group-item-label">{name}</span>
+class EmployeeListItem extends Component {
+  constructor(props: EmployeeProp) {
+    super(props);
 
-      <input
-        type="text"
-        className="list-group-item-input"
-        defaultValue={salary + '$'}
-      />
+    this.state = {
+      isIncrease: false,
+    };
 
-      <div className="d-flex justify-content-center align-items-center">
-        <button type="button" className="btn-cookie btn-sm">
-          <i className="fas fa-cookie"></i>
-        </button>
+    this.setIncrease = this.setIncrease.bind(this);
+    this.setChoosen = this.setChoosen.bind(this);
+  }
 
-        <button className="btn-trash btn-sm">
-          <i className="fas fa-trash"></i>
-        </button>
+  setIncrease() {
+    this.setState(({ isIncrease }: EmployeeState) => ({
+      isIncrease: !isIncrease,
+    }));
+  }
 
-        <i className="fas fa-star"></i>
-      </div>
-    </li>
-  );
+  setChoosen() {
+    this.setState(({ isChoosen }: EmployeeState) => ({
+      isChoosen: !isChoosen,
+    }));
+  }
+
+  render() {
+    const { name, salary } = this.props as EmployeeProp;
+    const { isIncrease, isChoosen } = this.state as EmployeeState;
+
+    const increaseClass = isIncrease ? ' increase' : '';
+    const choosenClass = isChoosen ? ' like' : '';
+
+    return (
+      <li
+        className={
+          'list-group-item d-flex justify-content-between' +
+          increaseClass +
+          choosenClass
+        }
+      >
+        <span className="list-group-item-label" onClick={this.setChoosen}>
+          {name}
+        </span>
+
+        <input
+          type="text"
+          className="list-group-item-input"
+          defaultValue={salary + '$'}
+        />
+
+        <div className="d-flex justify-content-center align-items-center">
+          <button
+            type="button"
+            className="btn-cookie btn-sm"
+            onClick={this.setIncrease}
+          >
+            <i className="fas fa-cookie"></i>
+          </button>
+
+          <button className="btn-trash btn-sm">
+            <i className="fas fa-trash"></i>
+          </button>
+
+          <i className="fas fa-star"></i>
+        </div>
+      </li>
+    );
+  }
 }
 
 export default EmployeeListItem;
